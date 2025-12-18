4c33f92bd6a2ff795f5e65ba4f1c05fad349015d
Reppan <72985260+Jepvid@users.noreply.github.com>
2025-12-18 02:04:44 +0100

    Update ResourceManagerHelpers.cpp

--- soh/soh/ResourceManagerHelpers.cpp
+++ soh/soh/ResourceManagerHelpers.cpp
@@ -425,10 +425,6 @@ void ResourceMgr_UnpatchGfxByName(const char* path, const char* patchName) {
         return;
     }
 
-    if (!ResourceMgr_IsAltAssetsEnabled()) {
-        rm->UnloadResource(path);
-    }
-    
     auto loaded = rm->LoadResource(path);
 
     // This can temporarily fail while toggling AltAssets.
