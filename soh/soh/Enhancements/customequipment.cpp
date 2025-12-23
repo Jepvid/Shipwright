bb4b93cd25b46a2fc6b480cb84fc0fc0c22dd3d7
Reppan <72985260+Jepvid@users.noreply.github.com>
2025-12-24 00:58:04 +0100

    Update customequipment.cpp

--- soh/soh/Enhancements/customequipment.cpp
+++ soh/soh/Enhancements/customequipment.cpp
@@ -4,7 +4,6 @@
 #include "objects/object_link_boy/object_link_boy.h"
 #include "objects/object_link_child/object_link_child.h"
 #include "objects/object_custom_equip/object_custom_equip.h"
-#include <fmt/format.h>
 #include "soh/Enhancements/game-interactor/GameInteractor.h"
 #include "soh/ShipInit.hpp"
 #include "soh/ResourceManagerHelpers.h"
