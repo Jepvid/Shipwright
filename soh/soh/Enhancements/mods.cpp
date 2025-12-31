b2be480be8311b8b4d48366fec297200155c745f
Reppan <72985260+Jepvid@users.noreply.github.com>
2025-12-31 10:57:48 +0100

    Update to latest and Updatehooks
    * updatehooks
    
    * fix crash

--- soh/soh/Enhancements/mods.cpp
+++ soh/soh/Enhancements/mods.cpp
@@ -5,8 +5,6 @@
 #include "soh/Enhancements/boss-rush/BossRush.h"
 #include "soh/Enhancements/enhancementTypes.h"
 #include <soh/Enhancements/item-tables/ItemTableManager.h>
-#include "soh/Enhancements/timesaver_hook_handlers.h"
-#include "soh/Enhancements/randomizer/hook_handlers.h"
 
 #include "src/overlays/actors/ovl_En_Bb/z_en_bb.h"
 #include "src/overlays/actors/ovl_En_Dekubaba/z_en_dekubaba.h"
